22c422260d358f8e756d2639c4fd3969b74adc5d
hdurante <53880710+hdurante@users.noreply.github.com>
2022-03-01 20:26:19 -0600

    Update crear_proyecto_angular.sh
    Se agrega funciones

--- crear_proyecto_angular.sh
+++ crear_proyecto_angular.sh
@@ -5,8 +5,32 @@ ng generate component people/people-list
 ng generate component people/people-add-edit
 ng generate component people/people-delete
 ng generate service shared
+echo ------------------------------------------------------
 echo #edita app/app.modules.ts para agregar 
 echo import {SharedService} from './shared.service'
 echo #y modificar providers: [], por
 echo providers: [SharedService],
-
+echo import {HttpClientModule} from '@angular/common/http'
+echo import { FormsModule,ReactiveFormsModule } from '@angular/forms'
+echo En componentes agregar
+echo HttpClientModule,
+echo FormsModule,
+echo ReactiveFormsModule,
+echo
+echo ------------------------------------------------------
+echo En shared.service.ts agregar
+echo  getPeopleList():Observable<any[]>{
+echo    return this.http.get<any[]>(this.urlApiPeoples+'/people/')
+echo  }
+echo 
+echo  addPeople(val:any){
+echo    return this.http.post(this.urlApiPeoples+'/people/',val)
+echo  }
+echo 
+echo  updatePeople(val:any){
+echo    return this.http.put(this.urlApiPeoples+'/people/',val)
+echo  }
+echo 
+echo  deletePeople(val:any){
+echo    return this.http.delete(this.urlApiPeoples+'/people/',val)
+echo  }
