bce5e5be1f632b1a4e538698a2d8b464d3f0219b
hdurante <53880710+hdurante@users.noreply.github.com>
2022-04-15 12:59:27 -0500

    Update instalar_docker_kuberness.sh

--- instalar_docker_kuberness.sh
+++ instalar_docker_kuberness.sh
@@ -10,22 +10,26 @@ sudo apt-get install \
     gnupg-agent \
     software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
 
-
 sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+apt-cache madison docker-ce
+#Seleccionar la que aplique y reemplazar
+
+sudo apt-get install docker-ce=<VERSION_STRING> docker-ce-cli=<VERSION_STRING> containerd.io
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
+
+sudo docker run hello-world
 
 #-----------------------------------------------------------
 sudo apt install apt-transport-https curl
