77ac66bab23488423d445249d1a253ee61623eae
hdurante <53880710+hdurante@users.noreply.github.com>
2022-04-15 13:03:32 -0500

    Update instalar_docker_kuberness.sh

--- instalar_docker_kuberness.sh
+++ instalar_docker_kuberness.sh
@@ -20,14 +20,6 @@ sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 
-apt-cache madison docker-ce
-#Seleccionar la que aplique y reemplazar
-
-sudo apt-get install docker-ce=<VERSION_STRING> docker-ce-cli=<VERSION_STRING> containerd.io
-
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-sudo chmod +x /usr/local/bin/docker-compose
 
 sudo docker run hello-world
 
