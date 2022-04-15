fc1646fa709cff9c2eb436ea8952a1c858b34348
hdurante <53880710+hdurante@users.noreply.github.com>
2022-04-15 11:58:00 -0500

    Update instalar_docker_kuberness.sh

--- instalar_docker_kuberness.sh
+++ instalar_docker_kuberness.sh
@@ -7,3 +7,30 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
+
+
+sudo apt install apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" >> ~/kubernetes.list
+sudo mv ~/kubernetes.list /etc/apt/sources.list.d
+
+sudo apt update
+
+
+sudo apt install kubelet
+sudo apt install kubeadm
+sudo apt install kubectl
+
+sudo apt-get install -y kubernetes-cni
+sudo apt-get install -y kubelet kubeadm kubectl kubernetes-cni
+
+sudo swapoff -a
+sudo nano /etc/fstab
+sudo hostnamectl set-hostname kubernetes-master
+
+sudo hostnamectl set-hostname kubernetes-worker
+lsmod | grep br_netfilter
+sudo modprobe br_netfilter
+
+sudo sysctl net.bridge.bridge-nf-call-iptables=1
