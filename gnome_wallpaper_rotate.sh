11b39a0424e7bab31a57a1569fd0406079fcf210
hdurante <53880710+hdurante@users.noreply.github.com>
2022-09-21 10:22:51 -0500

    Create gnome_wallpaper_rotate.sh
    Script que permite cambiar el wallpaper cada x minutos version 1 se ajustaran para recicibir parametros

---
+++ gnome_wallpaper_rotate.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# script to set random background wallpapers on my GNOME desktop
+# set base path
+#gsettings set org.gnome.desktop.background picture-uri "file:/home/user/wallpapers/file.jpg"
+export wallpaper_path=/home/user/wallpapers/
+shopt -s nullglob
+# store all the image file names in wallpapers array
+wallpapers=(
+    "$wallpaper_path"/*.jpg
+    "$wallpaper_path"/*.jpeg
+    "$wallpaper_path"/*.png
+    "$wallpaper_path"/*.bmp
+    "$wallpaper_path"/*.svg
+)
+# get array size
+wallpapers_size=${#wallpapers[*]}
+
+# set wallpapers in incremental order
+index=0
+while [ $index -lt $wallpapers_size ]
+do
+    gsettings set org.gnome.desktop.background picture-uri "file:${wallpapers[$index]}"
+    # index is maxing out, so reset it
+    if [ $(($index+1)) -eq $wallpapers_size ]
+    then
+        index=0
+    else
+        index=$(($index + 1))
+    fi
+    # keep the wallpaper for the specified time
+    sleep 15m
+done
