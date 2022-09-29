5de22a7ce78b3b917a0e106f73cd7313b1db340c
hdurante <53880710+hdurante@users.noreply.github.com>
2022-09-29 16:39:29 -0500

    Update crear_proyecto_angular.sh
    Se agrega ejemplo de chart.js

--- crear_proyecto_angular.sh
+++ crear_proyecto_angular.sh
@@ -38,3 +38,44 @@ echo
 echo  deletePeople(val:any){
 echo    return this.http.delete(this.urlApiPeoples+'/people/',val)
 echo  }
+echo ------------------------------------------------------
+echo Agregar Charts.js
+npm install chart.js ng2-charts
+echo agregar en app.modules.ts
+echo import { NgChartsModule } from 'ng2-charts';
+echo y en imports 
+echo NgChartsModule,
+echo en la vista agregar ViewChild al import de angular
+echo import { NgModule, Component, OnInit, ViewChild } from '@angular/core';
+echo @ViewChild('chartNombre') chartNombre: any;
+echo GeneraReporte() {
+echo     let canvas = this.chartNombre.nativeElement;
+echo     let ctx = canvas.getContext('2d');
+
+echo     new Chart(ctx, {
+echo       type: 'bar',
+echo       data: {
+echo         labels: ['Label1','Label2','Label3']
+echo         datasets: [{
+echo           label: 'Titulo Reporte',
+echo           data: ['Valor1','Valor2','Valor3'],
+echo           backgroundColor: [
+echo             'rgba(54, 162, 235, 0.2)'
+
+echo           ],
+echo           borderColor: [
+echo             'rgba(54, 162, 235, 1)'
+
+echo           ],
+echo           borderWidth: 1
+echo         }]
+echo       },
+echo       options: {
+echo         scales: {
+echo           y: {
+echo             beginAtZero: true
+echo           }
+echo         }
+echo       }
+echo     });
+echo   }
