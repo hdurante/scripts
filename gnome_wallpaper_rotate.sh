5d3abb023138252d5b62555e52ff0d9662d76775
hdurante <53880710+hdurante@users.noreply.github.com>
2022-10-22 16:07:57 -0500

    Update gnome_wallpaper_rotate.sh

--- gnome_wallpaper_rotate.sh
+++ gnome_wallpaper_rotate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # script to set random background wallpapers on my GNOME desktop
 # set base path
-#gsettings set org.gnome.desktop.background picture-uri "file:/home/user/wallpapers/file.jpg"
-export wallpaper_path=/home/user/wallpapers/
+#gsettings set org.gnome.desktop.background picture-uri "file:/mnt/hgfs/Proyectos/WallPapers/6277.jpg"
+export wallpaper_path=$1
 shopt -s nullglob
 # store all the image file names in wallpapers array
 wallpapers=(
@@ -16,17 +16,11 @@ wallpapers=(
 wallpapers_size=${#wallpapers[*]}
 
 # set wallpapers in incremental order
-index=0
+index=$(($RANDOM % $wallpapers_size))
 while [ $index -lt $wallpapers_size ]
 do
-    gsettings set org.gnome.desktop.background picture-uri "file:${wallpapers[$index]}"
-    # index is maxing out, so reset it
-    if [ $(($index+1)) -eq $wallpapers_size ]
-    then
-        index=0
-    else
-        index=$(($index + 1))
-    fi
+    gsettings set org.gnome.desktop.background picture-uri "file:${wallpapers[$index]}"    
+    index=$(( $RANDOM % $wallpapers_size ))    
     # keep the wallpaper for the specified time
     sleep 15m
 done
