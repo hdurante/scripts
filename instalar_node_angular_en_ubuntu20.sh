8ca51050667bc9519b005086f1a1cc3efe87eb49
hdurante <53880710+hdurante@users.noreply.github.com>
2022-10-22 16:09:44 -0500

    Se actualiza para que instale la version 18

--- instalar_node_angular_en_ubuntu20.sh
+++ instalar_node_angular_en_ubuntu20.sh
@@ -2,8 +2,8 @@ echo Se intala curl como requisito previo
 sudo apt-get install wget
 sudo apt install curl
 
-echo -------------- Se instalara nodejs 16, angular y firebase -------------------
-curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+echo -------------- Se instalara nodejs 18, angular y firebase -------------------
+curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt-get install nodejs
 node -v
 
