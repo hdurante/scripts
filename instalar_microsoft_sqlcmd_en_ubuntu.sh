e00ee49105817ddfcbb49329346ea340e428d0e3
hdurante <53880710+hdurante@users.noreply.github.com>
2023-03-28 11:22:46 -0600

    Se modifica al repositorio correcto 22.04
    Microsoft agrega repo 22.04 ya no es necesario usar el 20.04

--- instalar_microsoft_sqlcmd_en_ubuntu.sh
+++ instalar_microsoft_sqlcmd_en_ubuntu.sh
@@ -1,7 +1,7 @@
 sudo apt update
 sudo install curl
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
+curl https://packages.microsoft.com/config/ubuntu/22.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
 sudo apt-get update 
 sudo apt-get install mssql-tools unixodbc-dev
 sudo apt-get update 
