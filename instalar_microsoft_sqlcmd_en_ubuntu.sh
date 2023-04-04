08480fbd84f980a71e3dcdc1fc9555b8f6044ca8
hdurante <53880710+hdurante@users.noreply.github.com>
2023-04-03 20:52:48 -0600

    Se indica orden para dar prioridad a repositorios de Microsoft y evitar conflictos con su software

--- instalar_microsoft_sqlcmd_en_ubuntu.sh
+++ instalar_microsoft_sqlcmd_en_ubuntu.sh
@@ -2,6 +2,12 @@ sudo apt update
 sudo install curl
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/22.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
+echo ------- Se indica orden para dar prioridad a repositorios de Microsoft y evitar conflictos con su software
+sudo touch /etc/apt/preferences.d/99microsoft-dotnet.pref
+sudo echo Package: * >> /etc/apt/preferences.d/99microsoft-dotnet.pref
+sudo echo Pin: origin "packages.microsoft.com" >> /etc/apt/preferences.d/99microsoft-dotnet.pref
+sudo echo Pin-Priority: 1001 >> /etc/apt/preferences.d/99microsoft-dotnet.pref
+
 sudo apt-get update 
 sudo apt-get install mssql-tools unixodbc-dev
 sudo apt-get update 
