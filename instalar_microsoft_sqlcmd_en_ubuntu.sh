5a92b6ca51db3f0355fab3e74efdbd5a12d9dac3
hdurante <53880710+hdurante@users.noreply.github.com>
2024-05-08 16:34:13 -0600

    Update instalar_microsoft_sqlcmd_en_ubuntu.sh
    Se ajusta a 24.04 pero no queda del todo estable

--- instalar_microsoft_sqlcmd_en_ubuntu.sh
+++ instalar_microsoft_sqlcmd_en_ubuntu.sh
@@ -1,15 +1,19 @@
 sudo apt update
 sudo install curl
-curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-curl https://packages.microsoft.com/config/ubuntu/22.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
-echo ------- Se indica orden para dar prioridad a repositorios de Microsoft y evitar conflictos con su software
-sudo touch /etc/apt/preferences.d/99microsoft-dotnet.pref
-sudo echo Package: * >> /etc/apt/preferences.d/99microsoft-dotnet.pref
-sudo echo Pin: origin "packages.microsoft.com" >> /etc/apt/preferences.d/99microsoft-dotnet.pref
-sudo echo Pin-Priority: 1001 >> /etc/apt/preferences.d/99microsoft-dotnet.pref
+#curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+#curl https://packages.microsoft.com/config/ubuntu/22.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
+#echo ------- Se indica orden para dar prioridad a repositorios de Microsoft y evitar conflictos con su software
+#sudo touch /etc/apt/preferences.d/99microsoft-dotnet.pref
+#sudo echo Package: * >> /etc/apt/preferences.d/99microsoft-dotnet.pref
+#sudo echo Pin: origin "packages.microsoft.com" >> /etc/apt/preferences.d/99microsoft-dotnet.pref
+#sudo echo Pin-Priority: 1001 >> /etc/apt/preferences.d/99microsoft-dotnet.pref
+echo para Ubuntu 24.04
+wget https://packages.microsoft.com/config/ubuntu/24.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
 
 sudo apt-get update 
-sudo apt-get install mssql-tools unixodbc-dev
+sudo apt-get install unixodbc unixodbc-dev
 sudo apt-get update 
 sudo apt-get install mssql-tools
 export PATH="$PATH:/opt/mssql-tools/bin/"
